a1af199c651883e4c72ff48d4a7849687e27722a
beebopboop22 <bill@solar-states.com>
2025-08-04 16:07:42 -0400

    protect plans card and fix image export cors

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import LoginPage from './LoginPage.jsx'
 import SignUpPage from './SignUpPage.jsx'
 import ProfilePage from './ProfilePage.jsx';
 import PublicProfilePage from './PublicProfilePage.jsx';
-import UpcomingPlansCard from './UpcomingPlansCard.jsx';
+import ProtectedPlansCard from './ProtectedPlansCard.jsx';
 import { AuthProvider } from './AuthProvider.jsx'
 import MomentsExplorer from './MomentsExplorer.jsx' 
 import EventDetailPage from './EventDetailPage.jsx'
@@ -80,7 +80,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/u/:slug/plans-card" element={<UpcomingPlansCard />} />
+          <Route path="/u/:slug/plans-card" element={<ProtectedPlansCard />} />
           <Route path="/u/:slug" element={<PublicProfilePage />} />
           <Route path="/moments" element={<MomentsExplorer />} />
           <Route path="/moments/:id" element={<MomentsExplorer />} />
